e749053bef9647831858b439cca769d75d568e16
sunny shiva <sunnyshiva@sunnys-MacBook-Air.local>
2025-02-01 13:35:26 +0530

    feat: Add multilingual support for FAQs with auto-translation and dynamic retrieval

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import FAQ
 
-@admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    list_display = ('question', 'language')
+    list_display = ("question", "get_hindi_question", "get_bengali_question")
+    search_fields = ("question", "question_hi", "question_bn")
+
+    def get_hindi_question(self, obj):
+        return obj.question_hi or "Not Translated"
+    get_hindi_question.short_description = "Hindi Question"
+
+    def get_bengali_question(self, obj):
+        return obj.question_bn or "Not Translated"
+    get_bengali_question.short_description = "Bengali Question"
+
+admin.site.register(FAQ, FAQAdmin)
--- faqs/models.py
+++ faqs/models.py
@@ -1,24 +1,41 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-from django.core.cache import cache
 from googletrans import Translator
+
+translator = Translator()
 
 class FAQ(models.Model):
     question = models.TextField()
-    answer = RichTextField()  # WYSIWYG editor
-    language = models.CharField(max_length=5, choices=[('en', 'English'), ('hi', 'Hindi'), ('bn', 'Bengali')])
+    answer = RichTextField()
 
-    def get_translation(self, lang):
-        """Retrieve cached translation or generate if missing"""
-        cache_key = f"faq_{self.id}_lang_{lang}"
-        translation = cache.get(cache_key)
-        
-        if not translation:
-            translator = Translator()
-            translation = translator.translate(self.question, src='en', dest=lang).text
-            cache.set(cache_key, translation, timeout=86400)  # Cache for 1 day
-        
-        return translation
+    # Translations
+    question_hi = models.TextField(blank=True, null=True)  # Hindi
+    answer_hi = RichTextField(blank=True, null=True)
+
+    question_bn = models.TextField(blank=True, null=True)  # Bengali
+    answer_bn = RichTextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        """Auto-translate question and answer before saving."""
+        if not self.question_hi:
+            self.question_hi = translator.translate(self.question, src='en', dest='hi').text
+        if not self.answer_hi:
+            self.answer_hi = translator.translate(self.answer, src='en', dest='hi').text
+
+        if not self.question_bn:
+            self.question_bn = translator.translate(self.question, src='en', dest='bn').text
+        if not self.answer_bn:
+            self.answer_bn = translator.translate(self.answer, src='en', dest='bn').text
+
+        super().save(*args, **kwargs)
+
+    def get_translation(self, lang_code):
+        """Retrieve the translated question and answer dynamically."""
+        translations = {
+            "hi": (self.question_hi, self.answer_hi),
+            "bn": (self.question_bn, self.answer_bn),
+        }
+        return translations.get(lang_code, (self.question, self.answer))  # Default to English
 
     def __str__(self):
         return self.question
